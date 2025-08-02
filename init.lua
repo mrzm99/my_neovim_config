8a8ced461ce5000ef7c849626db3620013ed26dc
mrzm99 <mrzu.cpp@gmail.com>
2025-08-03 00:28:16 +0900

    add: set formatoptions

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 vim.hl = vim.highlight
 require("nvim_config.core")
 require("nvim_config.lazy")
-
+-- C言語ファイルを開いたときに自動コメント挿入を無効にする
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "c, c++, cpp, *.h",
+  callback = function()
+    vim.opt_local.formatoptions:remove({'r', 'o'})
+  end,
+})
