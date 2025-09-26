99fe2fe30aed159a9cac0050e1656d634f71cc00
mrzm99 <mrzu.cpp@gmail.com>
2025-09-27 00:51:47 +0900

    change colorscheme

--- lua/nvim_config/plugins/colorscheme.lua
+++ lua/nvim_config/plugins/colorscheme.lua
@@ -1,12 +1,20 @@
+--[[
 return {
     "tomasiser/vim-code-dark",
     lazy = false,
     config = function()
 
         vim.cmd("colorscheme codedark")
---        vim.api.nvim_set_hl(0, "Normal", { bg = "#171717" })
         vim.api.nvim_set_hl(0, "@keyword", { fg = "#D47EC0"})
         vim.api.nvim_set_hl(0, "Comment", { fg = "#808080" })
         vim.api.nvim_set_hl(0, "PreProc", { fg = "#FF99CC" })
+    end
+}
+--]]
+return {
+    "mhartington/oceanic-next",
+    lazy = false,
+    config = function()
+        vim.cmd("colorscheme OceanicNext")
     end
 }
