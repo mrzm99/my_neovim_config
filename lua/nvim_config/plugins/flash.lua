20546acea93a2586cec5e3a8699939249fe7edc9
mrzm99 <mrzu.cpp@gmail.com>
2025-10-03 22:51:45 +0900

    update

--- lua/nvim_config/plugins/flash.lua
+++ lua/nvim_config/plugins/flash.lua
@@ -3,9 +3,13 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").toggle() end, desc = "Toggle Flash search"},
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "flash treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "remote flash" },
+        { "r", mode = { "o", "x" }, function() require("flash").toggle() end, desc = "toggle flash search"},
     },
+    config = function()
+        vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#B00000", bg = "#1f1f1f", bold = true })
+        vim.api.nvim_set_hl(0, "FlashMatch", { fg = "#30D030", bg = "#1f1f1f", bold = true })
+    end
 }
