527f4fcdc102af4dcb7c0a971abec8e1e765fb86
mrzm99 <mrzu.cpp@gmail.com>
2025-11-30 12:24:43 +0900

    update

--- init.lua
+++ init.lua
@@ -5,8 +5,9 @@ require("nvim_config.lazy")
 
 -- C言語ファイルを開いたときに自動コメント挿入を無効にする
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "c, c++, cpp, *.h",
-  callback = function()
+    group = vim.api.nvim_create_augroup("C_Cpp_Settings", { clear = true }),
+    pattern = { "c", "cpp", "cs" },
+    callback = function()
     vim.opt_local.formatoptions:remove({'r', 'o'})
   end,
 })
